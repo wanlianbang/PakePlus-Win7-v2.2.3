444b177fe5930c004c3f96d1c68f1921b47c4b29
wanlianbang <978116158@qq.com>
2026-04-07 14:11:14 +0800

    Initial commit

---
+++ src-electron/custom.js
@@ -0,0 +1,41 @@
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    platform: process.platform,
+    versions: {
+        node: process.versions.node,
+        chrome: process.versions.chrome,
+        electron: process.versions.electron,
+    },
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('Preload script loaded')
+})
+
+console.log(
+    '%cbuild from PakePlus https://github.com/Sjj1024/PakePlus',
+    'color:orangered;font-weight:bolder'
+)
+
+// very important, if you don't know what it is, don't touch it
+// 非常重要，不懂代码不要动
+const hookClick = (e) => {
+    const origin = e.target?.closest('a')
+    const isBaseTargetBlank = document.querySelector(
+        'head base[target="_blank"]'
+    )
+    console.log('origin', origin, isBaseTargetBlank)
+    if (
+        (origin && origin.href && origin.target === '_blank') ||
+        (origin && origin.href && isBaseTargetBlank)
+    ) {
+        e.preventDefault()
+        console.log('handle origin', origin)
+        location.href = origin.href
+    } else {
+        console.log('not handle origin', origin)
+    }
+}
+
+document.addEventListener('click', hookClick, { capture: true })
